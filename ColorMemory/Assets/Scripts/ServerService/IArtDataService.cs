d0760f4b671b3fe94d4f0475b15463fa75910fe7
minkimgyu <48249824+minkimgyu@users.noreply.github.com>
2025-12-07 22:12:09 +0900

    Update engine version and rebuild asset bundle

--- ColorMemory/Assets/Scripts/ServerService/IArtDataService.cs
+++ ColorMemory/Assets/Scripts/ServerService/IArtDataService.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public interface IArtDataService
